50cc2dd8e2941852158b8afdd7009d006b1a376c
nanji <nanji@DESKTOP-4A8MIDK>
2022-11-03 22:36:18 +0300

    Add project files.

---
+++ Yard Greening/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double metre = double.Parse(Console.ReadLine());
+            double metrePrice = 7.61;
+            double price = metre * metrePrice;
+            double discount = 0.18 * price;
+            double finnalPrice = price - discount;
+
+            Console.WriteLine($"The finnal price is:{finnalPrice} lv.");
+            Console.WriteLine($"The discount is:{discount}");
+            
+        }
+    }
+}
